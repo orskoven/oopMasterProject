e69efd982e01b7e3b5c691fe8b95a2a281971df6
rasmuswoldike <rswoldike@gmail.com>
2021-10-06 09:55:56 +0200

    events

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         Guests peter = new Guests("peterpeter", "123peter", 2, "peter@gmail.com");
         ConcertAdmin lone = new ConcertAdmin("LoneR","lone123");
         Band javaMusic = new Band("javaMusic", 4,100000,true);
-        Event smukFest = new Event(22,10000,100,100,200,"henriette@hotmail.com","henriette","skanderborgvej 1",22,"skanderborg",10000);
+        Event smukFest = new Event(22,10000,10,100,200,"henriette@hotmail.com","henriette","skanderborgvej 1",22,"skanderborg",10000);
         Double smukFestPris = lone.getPrice(javaMusic.getStartPrice(), smukFest.getBandBonus(smukFest.getNumberOfGuests(), smukFest.getTicketPrice()));
         System.out.println(smukFestPris);
         Double bandSmukfestBonus = smukFest.getBandBonus(smukFest.getNumberOfGuests(), smukFest.getTicketPrice());
