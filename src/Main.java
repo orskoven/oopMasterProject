13f59ea75c4d1e7b4dcb57dc7b635a2c1fa3fb72
Simon Pedersen <90264457+orskoven@users.noreply.github.com>
2021-10-06 10:08:31 +0200

    band creator added

--- src/Main.java
+++ src/Main.java
@@ -21,17 +21,18 @@ public class Main {
         System.out.println("Your admin email: " + concertAdmin.createEmail());
 
         //Create
-
+        Simon.bandCreatorInterface();
 
         //Guests peter = new Guests("peterpeter","123peter");
         //ConcertAdmin lone = new ConcertAdmin("LoneR","lone123");
         Band javaMusic = new Band("javaMusic", 4,100000,true);
         Event smukFest = new Event(22,10000,100,100,200,"henriette@hotmail.com","henriette","skanderborgvej 1",22,"skanderborg",10000);
-        Double smukFestPris = lone.getPrice(javaMusic.getStartPrice(), smukFest.getBandBonus(smukFest.getNumberOfGuests(), smukFest.getTicketPrice()));
+        Double smukFestPris = concertAdmin.getPrice(javaMusic.getStartPrice(), smukFest.getBandBonus(smukFest.getNumberOfGuests(), smukFest.getTicketPrice()));
         System.out.println(smukFestPris);
         Double bandSmukfestBonus = smukFest.getBandBonus(smukFest.getNumberOfGuests(), smukFest.getTicketPrice());
         System.out.println(bandSmukfestBonus);
         System.out.println(smukFest.getEventRevenue(smukFestPris));
 
     }
+
 }
--- src/Simon.java
+++ src/Simon.java
@@ -1,8 +1,30 @@
+import java.util.Scanner;
+
 public class Simon {
     public Simon() {
     }
 
     public static void main(String[] args) {
+        }
+        public static boolean isContractApproved(String inputAnswerToContract) {
+        return inputAnswerToContract == "Y";
+
+
+        }
+        public static void bandCreatorInterface(){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Hello, let's add a band!\nWhat's the band name");
+            String bandName = scanner.nextLine();
+            System.out.println("How many members are there in the band?");
+            int bandMembers = scanner.nextInt();
+            System.out.println("What is the bands start price?");
+            int bandStartPrice = scanner.nextInt();
+            System.out.println("Is the band contract approved?Y/N");
+            System.out.println(scanner.nextLine());
+            String isContractApproved = scanner.nextLine();
+            Band newBand = new Band(bandName,bandMembers,bandStartPrice,isContractApproved(isContractApproved));
+
+        }
 
     }
-}
+
