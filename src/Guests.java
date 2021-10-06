d2638910b75f2802e9b470e9a558411274ebde16
Simon Pedersen <90264457+orskoven@users.noreply.github.com>
2021-10-06 09:11:22 +0200

    getters in BookingSystem

--- src/Guests.java
+++ src/Guests.java
@@ -1,6 +1,5 @@
 public class Guests extends User{
     public int numberOfTickets;
-
     public Guests(String email, String userName, String password) {
         super(email, userName, password);
     }
--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
         ConcertAdmin lone = new ConcertAdmin("lone@roskildefestivals.dk","LoneR","lone123");
         Band javaMusic = new Band("javaMusic",100000,true);
         Event smukFest = new Event(22,10000,100,100,200,"henriette@hotmail.com","henriette","skanderborgvej 1",22,"skanderborg",10000);
-        Double smukFestPris = lone.getPrice(javaMusic.getStartPrice(), smukFest.getBandBonus(smukFest.numberOfGuests, smukFest.ticketPrice));
+        Double smukFestPris = lone.getPrice(javaMusic.getStartPrice(), smukFest.getBandBonus(smukFest.getNumberOfGuests(), smukFest.getTicketPrice()));
         System.out.println(smukFestPris);
-        Double bandSmukfestBonus = smukFest.getBandBonus(smukFest.numberOfGuests, smukFest.ticketPrice);
+        Double bandSmukfestBonus = smukFest.getBandBonus(smukFest.getNumberOfGuests(), smukFest.getTicketPrice());
         System.out.println(bandSmukfestBonus);
         System.out.println(smukFest.getEventRevenue(smukFestPris));
 
