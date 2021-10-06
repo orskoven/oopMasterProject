d7264738618669a19ec089123866e3af82560b01
rasmuswoldike <rswoldike@gmail.com>
2021-10-06 09:25:08 +0200

    private

--- src/Band.java
+++ src/Band.java
@@ -1,10 +1,12 @@
 public class Band {
     private String bandName;
+    private int members;
     private int startPrice;
     private boolean isContract;
 
-    public Band(String bandName, int startPrice, boolean isContract) {
+    public Band(String bandName, int members, int startPrice, boolean isContract) {
         this.bandName = bandName;
+        this.members = members;
         this.startPrice = startPrice;
         this.isContract = isContract;
     }
