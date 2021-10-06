5284960f9bda46d8e5e7c100d0c7ac6509cd8e0a
Simon Pedersen <90264457+orskoven@users.noreply.github.com>
2021-10-06 09:29:47 +0200

    added email to adminclass

--- src/ConcertAdmin.java
+++ src/ConcertAdmin.java
@@ -1,3 +1,6 @@
+import java.util.Locale;
+import java.util.Random;
+
 public class ConcertAdmin extends User{
     public ConcertAdmin(String email, String userName, String password) {
         super(email, userName, password);
@@ -5,5 +8,24 @@ public class ConcertAdmin extends User{
 
     public double getPrice(double bandStartPrice, double eventPrice){
         return bandStartPrice + eventPrice;
+    }
+
+    public String createEmail() {
+        return getInitials(getUserName()) + getRandomNumber(0,9) + "@event-booking";
+    }
+
+    public int getRandomNumber(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(max - min) + max;
+    }
+
+    public String getInitials(String studentName) {
+        String reversed = "";
+        for (int i = 0; i+getUserName().length()-1 <= getUserName().length(); i++) {
+            char k = (char) (getUserName().charAt(i) );
+            reversed += k;
+        }
+        return reversed.toLowerCase(Locale.ROOT);
+
     }
 }
