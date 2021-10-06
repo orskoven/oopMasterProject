7aa9e410c6ea8b59b501be6c20d19ef3d656e722
Simon Pedersen <90264457+orskoven@users.noreply.github.com>
2021-10-06 10:19:39 +0200

    added number of bands to create

--- src/Simon.java
+++ src/Simon.java
@@ -14,17 +14,21 @@ public class Simon {
         }
         public static void bandCreatorInterface(){
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Hello, let's add a band!\nWhat's the band name");
-            String bandName = scanner.nextLine();
-            System.out.println("How many members are there in the band?");
-            int bandMembers = scanner.nextInt();
-            System.out.println("What is the bands start price?");
-            int bandStartPrice = scanner.nextInt();
-            System.out.println("Is the band contract approved?Y/N");
-            System.out.println(scanner.nextLine());
-            String isContractApproved = scanner.nextLine();
-            Band newBand = new Band(bandName,bandMembers,bandStartPrice,isContractApproved(isContractApproved));
-
+            System.out.println("How many bands do you need to create?");
+            int numberOfBands = scanner.nextInt();
+            for (int i = 0; i < numberOfBands; i++) {
+                System.out.println("Let's add a band!\nWhat's the band name");
+                System.out.println(scanner.nextLine());
+                String bandName = scanner.nextLine();
+                System.out.println("How many members are there in the band?");
+                int bandMembers = scanner.nextInt();
+                System.out.println("What is the bands start price?");
+                int bandStartPrice = scanner.nextInt();
+                System.out.println("Is the band contract approved?Y/N");
+                System.out.println(scanner.nextLine());
+                String isContractApproved = scanner.nextLine();
+                Band newBand = new Band(bandName, bandMembers, bandStartPrice, isContractApproved(isContractApproved));
+            }
         }
 
     }
