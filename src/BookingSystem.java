07c0110dcac3f502432db18b66dc6ffa9eeefd25
Simon Pedersen <90264457+orskoven@users.noreply.github.com>
2021-10-06 09:08:39 +0200

    getters in BookingSystem

--- src/BookingSystem.java
+++ src/BookingSystem.java
@@ -1,11 +1,18 @@
 public class BookingSystem {
-    public int time;
-    public int numberOfGuests;
-    public double ticketPrice;
+    private int time;
+    private int numberOfGuests;
+    private double ticketPrice;
 
     public BookingSystem(int time, int numberOfGuests, double ticketPrice) {
         this.time = time;
         this.numberOfGuests = numberOfGuests;
         this.ticketPrice = ticketPrice;
+    }
+
+    public int getNumberOfGuests(){
+        return numberOfGuests;
+    }
+    public double getTicketPrice(){
+        return ticketPrice;
     }
 }
